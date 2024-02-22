105240832b92ae44f122ed2c6b424ddaffb690f7
WtfAno <157568086+WtfAno@users.noreply.github.com>
2024-02-22 20:22:31 +0530

    Update config.py
    Signed-off-by: WtfAno <157568086+WtfAno@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://graph.org/file/644bbf0c41c2bd6cd0cde.mp4",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
